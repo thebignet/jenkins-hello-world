743997eb3dd9b5496bb65e6d4c2f79455d424847
Jean Detoeuf <jdetoeuf.ext@orange.com>
2015-09-03 14:42:13 +0200

    Added integration test

---
+++ src/test/java/org/detoeuf/helloworld/MainControllerIT.java
@@ -0,0 +1,35 @@
+package org.detoeuf.helloworld;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = Application.class)
+@WebAppConfiguration
+public class MainControllerIT {
+  @Autowired
+  private MainController mainController;
+
+  private MockMvc mockMvc;
+
+  @Before
+  public void setup() {
+    this.mockMvc = MockMvcBuilders.standaloneSetup(mainController).build();
+  }
+
+  @Test
+  public void pageShouldLoadCorrectly() throws Exception {
+    mockMvc.perform(get("/").accept(MediaType.TEXT_HTML)).andExpect(status().isOk());
+  }
+}
